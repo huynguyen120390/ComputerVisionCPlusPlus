f58d08fc190895ea3728d3a236bcf082d7059af6
Huy H. Nguyen <huynguyen120390@gmail.com>
2019-09-01 02:09:59 -0700

    StartUp

---
+++ ReadingWritingImages/ReadingWritingImages/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  ReadingWritingImages
+//
+//  Created by Huy Nguyen on 9/1/19.
+//  Copyright © 2019 Huy Nguyen. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    Mat colorImage = imread("/Users/huynguyen/CodeLab/GitProjects/ComputerVisionCPlusPlus/ReadingWritingImages/Image/dog.jpg");
+    
+    if(!colorImage.data){
+        cout << "Cannot open Image\n";
+        return -1;
+    }
+    
+    imwrite("/Users/huynguyen/CodeLab/GitProjects/ComputerVisionCPlusPlus/ReadingWritingImages/Image/coloredDog.jpg",colorImage);
+    imshow("Color Dog",colorImage);
+    
+    return 0;
+}
